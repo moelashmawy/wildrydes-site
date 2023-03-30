62c7f73fb55fc454c9f61fdc366f3398fc0f9fc8
moelashmawy <ashmawy894@gmail.com>
2023-03-30 19:10:12 +0200

    new_config

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'eu-north-1_Iebb1hF34', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '64b23g158eej2n4hvsrcgnmcnd', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-north-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
